5fbddc83e0724ece6e890bd634d5c89dcc6a3353
TJ Klimoski <tjklimoski@gmail.com>
2024-03-03 20:05:56 -0500

    Create get function to list all words in the board

--- src/Generator.ts
+++ src/Generator.ts
@@ -8,6 +8,7 @@ export class Generator {
   // wordsInBoard is a getter function
   #history: string[];
   #board: Board;
+  #boardSize: number = 3;
 
   constructor() {
     this.#history = [];
@@ -16,14 +17,34 @@ export class Generator {
 
   #createBoard() {
     const board: Board = [null!, null!, null!];
-    for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+    for (let rowIndex = 0; rowIndex < this.#boardSize; rowIndex++) {
       const row: BoardRow = [null!, null!, null!];
-      for (let colIndex = 0; colIndex < 3; colIndex++) {
+      for (let colIndex = 0; colIndex < this.#boardSize; colIndex++) {
         const letter = new Letter(`${rowIndex}${colIndex}`);
         row.push(letter);
       }
       board.push(row);
     }
     return board;
+  }
+
+  get wordsInBoard(): string[] {
+    const words: string[] = [];
+
+    for (let i = 0; i < this.#boardSize; i++) {
+      const rowWord = this.#board[i].map(letter => letter.value).join("");
+      let colWord = "";
+
+      // loop through rows to get each letter in a colWord
+      for (const row of this.#board) {
+        const letter = row.map(letter => letter.value)[i];
+        if (letter) colWord += letter;
+      }
+
+      if (rowWord.length === 3) words.push(rowWord);
+      if (colWord.length === 3) words.push(colWord);
+    }
+
+    return words;
   }
 }
